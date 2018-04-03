ff33430be526b0ad4b24263ad92784699133f8b2
Evans Hauser <evanshauser@gmail.com>
2018-04-02 23:03:07 -0700

    adding documentation to proposal

--- src/schema.ts
+++ src/schema.ts
@@ -43,27 +43,29 @@ const createLoaderDirective = (load: DataLoader.BatchLoadFn<any, any>) => {
       const defaultResolver = (res: any) => res;
       const resolve = field.resolve || defaultResolver;
 
-      field.resolve = (root, args, context, info: any) => {
+      field.resolve = (root, args, context, info) => {
         let key;
+        console.log(info.rootValue);
+        console.log(info);
 
         if (this.args.id) {
           const id = this.args.id as string;
-          // key.replace(':', '$');
           const test = /({(.*?)}.?)+/;
           const keys = [];
-          // let result = regex.exec(key);
-          // while (result) {
-          //   keys.push(result[1]);
-          //   result = regex.exec(key);
-          // }
           const str = id.split(regex);
-          // console.log('split', str);
           key = str
             .map((elem, i) => {
               if (i % 2 === 1) {
                 console.log('elem', elem, root[elem], args[elem]);
                 if (elem.startsWith('$')) {
                   return args[elem.substring(1)];
+                } else if (
+                  elem.startsWith('root.') ||
+                  elem.startsWith('args.') ||
+                  elem.startsWith('context.') ||
+                  elem.startsWith('info.')
+                ) {
+                  return eval(elem);
                 } else {
                   return root[elem] || args[elem];
                 }
@@ -78,7 +80,7 @@ const createLoaderDirective = (load: DataLoader.BatchLoadFn<any, any>) => {
         } else if (this.args.context) {
           key = context[this.args.context];
         } else if (this.args.info) {
-          key = info[this.args.info];
+          key = (info as any)[this.args.info];
         }
 
         if (Array.isArray(key)) {
@@ -90,46 +92,6 @@ const createLoaderDirective = (load: DataLoader.BatchLoadFn<any, any>) => {
             .load(key)
             .then(val => resolve(val, args, context, info));
         }
-
-        // console.log('key', key);
-        // if (field.astNode) {
-        //   switch (field.astNode.type.kind) {
-        //     case 'ListType':
-        //       return instance
-        //         .loadMany(key || ['res_0'])
-        //         .then(res => {
-        //           console.log('load', res);
-        //           return res;
-        //         })
-        //         .then(val => resolve(val, args, context, info));
-        //     case 'NamedType':
-        //       return instance
-        //         .load(key || 'res_1')
-        //         .then(res => {
-        //           console.log('load', res);
-        //           return res;
-        //         })
-        //         .then(val => resolve(val, args, context, info));
-        //     case 'NonNullType':
-        //       if (field.astNode.type.type.kind === 'ListType') {
-        //         return instance
-        //           .loadMany(key || ['res_0'])
-        //           .then(res => {
-        //             console.log('load', res);
-        //             return res;
-        //           })
-        //           .then(val => resolve(val, args, context, info));
-        //       } else {
-        //         return instance
-        //           .load(key || 'res_1')
-        //           .then(res => {
-        //             console.log('load', res);
-        //             return res;
-        //           })
-        //           .then(val => resolve(val, args, context, info));
-        //       }
-        //   }
-        // }
       };
     }
   };
@@ -152,29 +114,52 @@ const typeDefs = gql`
   }
 
   type User {
-    id: ID! @deprecated(reason: "too lit")
-    name: String @deprecated
-    posts: [Post]! @load(root: "postsIds")
-    game: Game! @load(args: "gameId")
+    id: ID!
+    name: String
 
-    #pulls from root value first, then from the arguments, unless prefixed with $
-    fortune: Fortune! @get(id: "fortunes/{id}")
-    fortune_(lol: String!): Fortune! @get(id: "fortunes/{lol}")
-    fortune_arg(id: String!): Fortune! @get(id: "fortunes/{$id}")
+    #The directives here are used to generate the dataloader ids
 
+    #OPTION 1
+    #The key inside of {} is references root value first, then the arguments, unless prefixed with $
+    v1_fortune: Fortune! @get(id: "fortunes/{id}")
+    v1_fortune_var(var: String!): Fortune! @get(id: "fortunes/{var}")
+    v1_fortune_arg(id: String!): Fortune! @get(id: "fortunes/{$id}")
+
+    #The ids passed to dataloader for ^ are:
+    #id = fortunes/\${root.id}
+    #id = fortunes/\${args.var}
+    #id = fortunes/\${args.id}
+
+    #OPTION 2
+    #We explicitly provide where we generate the id passed to dataloader
+    v2_fortune_root: Fortune! @get(id: "fortunes/{root.id}")
+    v2_fortune_args(id: String!): Fortune! @get(id: "fortunes/{args.id}")
+    v2_fortune_context: Fortune! @get(id: "fortunes/{context.id}")
+    v2_fortune_info: Fortune! @get(id: "fortunes/{info.id}")
     #\${} unfortunately fails to lex
-    fortune_root: Fortune! @get(id: "fortunes/{root.id}")
-    fortune_args(id: String!): Fortune! @get(id: "fortunes/{args.id}")
-    fortune_context: Fortune! @get(id: "fortunes/{context.id}")
-    fortune_info: Fortune! @get(id: "fortunes/{info.id}")
 
+    #The ids passed to dataloader for ^ are:
+    #id = fortunes/\${root.id}
+    #id = fortunes/\${args.id}
+    #id = fortunes/\${context.id}
+    #id = fortunes/\${info.id}
+
+    #OPTION 2
     #These would be directly referenced: i.e. root.id
-    fortune_root: Fortune! @get(root: "id")
-    fortune_args(id: String!): Fortune! @get(args: "id")
-    fortune_context: Fortune! @get(context: "id")
-    fortune_info: Fortune! @get(id: "id")
+    v3_fortune_root: Fortune! @get(root: "id")
+    v3_fortune_args(id: String!): Fortune! @get(args: "id")
+    v3_fortune_context: Fortune! @get(context: "id")
+    v3_fortune_info: Fortune! @get(id: "id")
+
+    #The ids passed to dataloader for ^ are:
+    #id = \${root.id}
+    #id = \${args.var}
+    #id = \${args.id}
 
     fortunes: [Fortune]! @get(id: "fortunes")
+
+    posts: [Post]! @load(root: "postsIds")
+    game: Game! @load(args: "gameId")
   }
 
   type Query {
@@ -190,23 +175,6 @@ const resolvers: IResolvers = {
       return Promise.resolve(users);
     },
   },
-  User: {
-    // posts: root => root,
-    // fortunes: root => root,
-    // fortunes: () => {
-    //   return fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
-    //     .then(res => res.json())
-    //     .then(res => {
-    //       return res[0].fortune.message;
-    //     });
-    // },
-  },
-  Fortune: {
-    // msg: msg => {
-    //   // console.trace();
-    //   return msg;
-    // },
-  },
 };
 
 const findByIds = (ids: Array<any>) => {
@@ -220,20 +188,14 @@ const fetcher = (url: string) => (ids: any[]) => {
   //could check if result is a String and just get text, otherwise return json
   return Promise.all(
     ids.map(id => {
-      return (
-        fetch(url + id)
-          .then(res => res.text())
-          // .then(res => {
-          //   console.log(res);
-          //   return res;
-          // })
-          .then(res => JSON.parse(res))
-          .then(res => {
-            if (!res.id) res.id = id;
-            // console.log('fetched', res);
-            return res;
-          })
-      );
+      return fetch(url + id)
+        .then(res => res.text())
+        .then(res => JSON.parse(res))
+        .then(res => {
+          if (!res.id) res.id = id;
+          // console.log('fetched', res);
+          return res;
+        });
     }),
   );
 };
@@ -247,9 +209,6 @@ export const schema = makeExecutableSchema({
     get: createLoaderDirective(
       fetcher('http://fortunecookieapi.herokuapp.com/v1/'),
     ),
-    log: DebugDirective,
-    use: UseDirective,
-    stack: TraceDirective,
   },
 });
 
