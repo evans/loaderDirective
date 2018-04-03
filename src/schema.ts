71bbb7a239ddced8c9b1ffc5003639a3658b3584
Evans Hauser <evanshauser@gmail.com>
2018-04-02 23:16:04 -0700

    remove extra information

--- src/schema.ts
+++ src/schema.ts
@@ -45,9 +45,8 @@ const createLoaderDirective = (load: DataLoader.BatchLoadFn<any, any>) => {
 
       field.resolve = (root, args, context, info) => {
         let key;
-        console.log(info.rootValue);
-        console.log(info);
 
+        //This serves OPTION 1 and 2
         if (this.args.id) {
           const id = this.args.id as string;
           const test = /({(.*?)}.?)+/;
@@ -56,16 +55,17 @@ const createLoaderDirective = (load: DataLoader.BatchLoadFn<any, any>) => {
           key = str
             .map((elem, i) => {
               if (i % 2 === 1) {
-                console.log('elem', elem, root[elem], args[elem]);
-                if (elem.startsWith('$')) {
-                  return args[elem.substring(1)];
-                } else if (
+                //OPTION 2
+                if (
                   elem.startsWith('root.') ||
                   elem.startsWith('args.') ||
                   elem.startsWith('context.') ||
                   elem.startsWith('info.')
                 ) {
                   return eval(elem);
+                  //OPTION 1
+                } else if (elem.startsWith('$')) {
+                  return args[elem.substring(1)];
                 } else {
                   return root[elem] || args[elem];
                 }
@@ -73,6 +73,8 @@ const createLoaderDirective = (load: DataLoader.BatchLoadFn<any, any>) => {
               return elem;
             })
             .join('');
+
+          //OPTION 3
         } else if (this.args.root) {
           key = root[this.args.root];
         } else if (this.args.args) {
@@ -103,11 +105,6 @@ const typeDefs = gql`
     title: String
   }
 
-  type Game {
-    id: ID!
-    title: String
-  }
-
   type Fortune {
     id: ID!
     msg: String @use(key: "message")
@@ -117,7 +114,8 @@ const typeDefs = gql`
     id: ID!
     name: String
 
-    #The directives here are used to generate the dataloader ids
+    #The directives here are used to generate the ids passed to dataloader
+    #When dataloader is run, the returned value will be passed as the rootValue to the resolver function
 
     #OPTION 1
     #The key inside of {} is references root value first, then the arguments, unless prefixed with $
@@ -132,11 +130,10 @@ const typeDefs = gql`
 
     #OPTION 2
     #We explicitly provide where we generate the id passed to dataloader
-    v2_fortune_root: Fortune! @get(id: "fortunes/{root.id}")
+    v2_fortune_root: Fortune! @get(id: "fortunes/{root.id.hi}")
     v2_fortune_args(id: String!): Fortune! @get(id: "fortunes/{args.id}")
     v2_fortune_context: Fortune! @get(id: "fortunes/{context.id}")
     v2_fortune_info: Fortune! @get(id: "fortunes/{info.id}")
-    #\${} unfortunately fails to lex
 
     #The ids passed to dataloader for ^ are:
     #id = fortunes/\${root.id}
@@ -144,22 +141,24 @@ const typeDefs = gql`
     #id = fortunes/\${context.id}
     #id = fortunes/\${info.id}
 
-    #OPTION 2
+    #OPTION 3
     #These would be directly referenced: i.e. root.id
     v3_fortune_root: Fortune! @get(root: "id")
     v3_fortune_args(id: String!): Fortune! @get(args: "id")
     v3_fortune_context: Fortune! @get(context: "id")
-    v3_fortune_info: Fortune! @get(id: "id")
+    v3_fortune_info: Fortune! @get(info: "id")
 
     #The ids passed to dataloader for ^ are:
     #id = \${root.id}
-    #id = \${args.var}
     #id = \${args.id}
+    #id = \${context.id}
+    #id = \${info.id}
+
+    #Notes
+    #\${} unfortunately fails to lex
 
     fortunes: [Fortune]! @get(id: "fortunes")
-
     posts: [Post]! @load(root: "postsIds")
-    game: Game! @load(args: "gameId")
   }
 
   type Query {
@@ -185,7 +184,7 @@ const findByIds = (ids: Array<any>) => {
 
 const fetcher = (url: string) => (ids: any[]) => {
   console.log(`Find by ids ${ids}.`);
-  //could check if result is a String and just get text, otherwise return json
+  //could check if field requested is a String and just get text, otherwise return json
   return Promise.all(
     ids.map(id => {
       return fetch(url + id)
@@ -209,13 +208,6 @@ export const schema = makeExecutableSchema({
     get: createLoaderDirective(
       fetcher('http://fortunecookieapi.herokuapp.com/v1/'),
     ),
+    use: UseDirective,
   },
 });
-
-export function context() {
-  return {
-    // Intitialize the dataloader with the batch function.
-    // Note, we create a new DataLoader per request to ensure the cache is flushed.
-    loader: new DataLoader(findByIds),
-  };
-}
